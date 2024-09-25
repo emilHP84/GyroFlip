98af3d53e6beb9263997318af70b507f7f9be470
emil <ehp84@sfr.fr>
2024-09-25 15:52:27 +0200

    ajout timer & flip (GameManager)

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -11,11 +11,12 @@ public class GameManager : MonoBehaviour
 
     public GameObject targetedtEntity;
 
-    private float timer;
+    private float timer = 0;
+    private float dTime = 8;
 
     public void OnEnable()
     {
-        
+
     }
 
     public void Awake()
@@ -24,15 +25,40 @@ public class GameManager : MonoBehaviour
     }
     public void Start()
     {
-        
+
+    }
+    public delegate void OnFlipDelegate();
+    public event OnFlipDelegate OnFlip;
+    private void Flip() 
+    {
+        OnFlip?.Invoke();
     }
 
     public void Update()
     {
+        TimerDecrement();
+    }   
 
+    private void TimerDecrement()
+    {
+        timer += Time.deltaTime;
+        if (timer > dTime)
+        {
+            if (dTime > 2.5f)
+            {
+                timer = 0;
+                dTime -= 0.2f;
+                Flip();
+            }
+            else
+            {
+                timer = 0;
+                Flip();
+            }
+        }
     }
     public void OnDisable()
     {
-        
+
     }
 }
