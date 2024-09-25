b1abb0333076f37365d739b1913f80a3e122a0e2
emil <ehp84@sfr.fr>
2024-09-25 11:02:33 +0200

    ajout ennemiManager

---
+++ Assets/Script/Manager/EnnemiManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemiManager : MonoBehaviour
+{
+    public void Start()
+    {
+        
+    }
+    public void Update()
+    {
+        
+    }
+}
