5eb8b992931ef7e4c71e717d5645d89a08ea90b0
emil <ehp84@sfr.fr>
2024-09-25 10:53:01 +0200

    ajout gameManager

---
+++ Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool InGame = false;
+    public static bool IsDead = false;
+
+    private float timer;
+
+    public void OnEnable()
+    {
+        
+    }
+    public void Start()
+    {
+        
+    }
+    public void Update()
+    {
+
+    }
+    public void OnDisable()
+    {
+        
+    }
+}
