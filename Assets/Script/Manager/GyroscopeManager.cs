02a08b4bfb4a8e1aed5a6d8a70c48520f0ef4be5
emil <ehp84@sfr.fr>
2024-09-25 15:36:03 +0200

    gyroscope implémenté (non définitif)

---
+++ Assets/Script/Manager/GyroscopeManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroscopeManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    void Update()
+    {
+        Debug.Log(Input.gyro.attitude);
+        transform.rotation = Input.gyro.attitude;
+    } 
+    
+
+
+}
