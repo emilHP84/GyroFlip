6d5689884470d43765201fbd1ec3cfb81d31af17
Linkdu <chaillouxmarius@gmail.com>
2024-10-01 11:33:57 +0200

    update square full white + début Flip_Warning (pas fonctionel)

--- Assets/Flip_Warning.cs
+++ Assets/Flip_Warning.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Flip_Warning : MonoBehaviour
 {
-    
+    public GameObject Up_right;
+    public GameObject Up_left;
+    public GameObject Down_right;
+    public GameObject Down_left;
+
     void Start()
     {
         
@@ -13,6 +18,13 @@ public class Flip_Warning : MonoBehaviour
     
     void Update()
     {
+        movimiento();
+    }
+
+    void movimiento()
+    {
+        Down_left.transform.DOScaleY(1.3f, 0.1f).SetLoops(2,LoopType.Yoyo);
+        Down_left.transform.DOScaleY(-1.3f, -0.1f).SetLoops(2,LoopType.Yoyo);
         
     }
 }
